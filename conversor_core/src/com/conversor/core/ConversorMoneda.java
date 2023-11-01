2dd2b4dc581c292b8b55742070cd6e556f7b036c
Erick Rivera <110272462+ErickRiver@users.noreply.github.com>
2023-10-31 23:40:10 -0600

    Delete conversor_core directory

--- conversor_core/src/com/conversor/core/ConversorMoneda.java
+++
@@ -1,9 +0,0 @@
-package com.conversor.core;
-
-public class ConversorMoneda {
-    
-    public double convertirCantidad(double precioDivisa, double cantidad) {   
-        return precioDivisa * cantidad;
-    }
-    
-}
